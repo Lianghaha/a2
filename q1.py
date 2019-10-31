3cafe8d61229749c41f136d2df233fc346fd595a
Lianghaha <jialiang0601@gmail.com>
2019-10-31 19:39:49 -0400

    modify answer file

--- q1.py
+++ q1.py
@@ -48,8 +48,15 @@ What N V Adv PP
 What should N V DET N Adv PP
 Where V DET N Adv PP
 """
-sentence = nltk.tokenize.word_tokenize(""" where should people walk their dogs quickly """.lower())
+file = open("Q3.1Negative.txt", "r")
+#file = open("Q3.1Positive.txt", "r")
 parser = nltk.parse.BottomUpChartParser(grammar)
-for t in parser.parse_all(sentence):
-    print(t)
+for line in file:
+    sentence = nltk.tokenize.word_tokenize(line)
+    result = parser.parse_all(sentence)
+    print("sentence: " + line.rstrip())
+    print("Num of results: " + str(len(result)))
+    for item in result:
+        print(item)
+    print("\n")
 
