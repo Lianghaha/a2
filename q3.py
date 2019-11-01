8a1ea2a54306cc2421c4b22b7a3debe8f621b398
Lianghaha <jialiang0601@gmail.com>
2019-11-01 10:21:49 -0400

    created all files

--- q3.py
+++ q3.py
@@ -1,14 +1,40 @@
 import nltk
 
+
+def loop_over_file(file, flag):
+    total = 0
+    correct = 0
+    for line in file:
+        print("")
+        sentence = nltk.tokenize.word_tokenize(line)
+        result = parser.parse_all(sentence)
+        total += 1
+        if len(result) > 0:
+            correct += 1
+        print("sentence: " + line.rstrip())
+        print("Num of results: " + str(len(result)))
+        for item in result:
+            print(item)
+    if flag:
+        print("---------------------------------Positive Done ---------------------------------")
+    else:
+        print("--------------------------------------------------------------------------------")
+
+    return total, correct
+
+
 grammar_text = open("Grammar.txt", "r").read() + open("Lexicon.txt", "r").read()
 grammar = nltk.grammar.CFG.fromstring(grammar_text)
 
-file = open("q3 Negative.txt", "r")
-#file = open("q3 Positive.txt", "r")
+file_Negative = open("q3 Negative.txt", "r")
+file_Positive = open("q3 Positive.txt", "r")
 parser = nltk.parse.BottomUpChartParser(grammar)
-total = 0
-correct = 0
 
+P_total, P_correct = loop_over_file(file_Positive, True)
+N_total, N_correct = loop_over_file(file_Negative, False)
+print("\nPositive: Total: {}, Correct: {}".format(P_total, P_correct))
+print("\nNegative: Total: {}, Correct: {}".format(N_total, N_correct))
+'''
 for line in file:
     print("")
     sentence = nltk.tokenize.word_tokenize(line)
@@ -20,8 +46,10 @@ for line in file:
     print("Num of results: " + str(len(result)))
     for item in result:
         print(item)
+'''
 
-print("\nTotal: {}, Correct: {}".format(total, correct))
+
+
 
 # TODO
 # Grammar, Lexicon, Positive, Negative, Overgen, Undergen
